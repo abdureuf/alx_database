d77a168746e0e9d0951ef149049da9c224010401
abdureufshifa <shifabdureuf@gmail.com>
2023-08-16 23:14:38 +0300

    SQL_introduction1

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,2 @@
--- the database uses the mysql database from list of database created by default and use keyword USE then place the database name
-USE mysql;
 -- to see all tables within specified database to use show keyword and tables
 SHOW TABLES;
