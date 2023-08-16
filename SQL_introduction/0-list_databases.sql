df35a251a77ac9acd0877b7f29ba59756d74e3c5
abdureufshifa <shifabdureuf@gmail.com>
2023-08-16 22:10:17 +0300

    list_database

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,4 @@
-/* - use SHOW statement  to see created by default
-   - use DATABASES to specify from table then use ; after all end of code
-*/
+-- use SHOW statement  to see created by default
+-- use DATABASES to specify from table then use ; after all end of code
+
 SHOW DATABASES;
