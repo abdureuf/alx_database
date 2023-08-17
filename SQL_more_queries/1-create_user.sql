591c424f8d5b607600d4ad522e7fab0c41447f46
abdureufshifa <shifabdureuf@gmail.com>
2023-08-17 21:23:43 +0300

    SQL_more_queries_last

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 CREATE USER IF NOT EXISTS'user_0d_1'@'localhost';
 -- use SET PASSWORD FOR for asign  the password
 SET PASSWORD FOR 'user_0d_1'@'localhost'="user_0d_1_pwd";
+-- all privilages for user_0d_1  from sql server and *.* means all databases with all tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,10 @@
--- cities.* -means select all column in cities table
--- cities, states - from two tables
-SELECT cities.* 
-FROM cities, states 
-WHERE cities.state_id = states.id 
-AND states.name = 'California' 
-ORDER BY cities.id ASC;
+-- * -means select all column in cities table
+-- by using subquery
+SELECT *
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC
